738396800329e017b7a5437752008a2c57e764f2
SmartWebDeveloper10 <sashaeliseev81@gmail.com>
2022-03-22 20:47:55 +0300

    ingredient columns and callback work

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -18,7 +18,7 @@ export const comparatorBurger = (a : any,b: any)=>{
 
 export const getSortedData =  (getData: any ,comparator: any  ) :any =>{
     const data = getData();
-    const sortedData = data.sort(comparator);
+    const sortedData = data;//.sort(comparator);
     
     var types = new Set(); var ingredientMap = new Map(); const resultArr : any=[];
 
@@ -36,29 +36,7 @@ export const getSortedData =  (getData: any ,comparator: any  ) :any =>{
 
 export const BurgerConstructor =(props: any)=>{
     const [current, setCurrent] = useState('one')
-
-
-    const ref1 = useRef<HTMLDivElement>(null);
-    const ref2 = useRef<HTMLDivElement>(null);
-    const ref3 = useRef<HTMLDivElement>(null);
-
-    const refMap = new Map();
-
-
-    const getToOne = (e:any)=>{
-        setCurrent("one");
-        ref1.current?.scrollIntoView({behavior:"smooth"});
-    }
-
-    const getToTwo = (e:any)=>{
-        setCurrent("two");
-        ref2.current?.scrollIntoView({behavior:"smooth"});
-    }
-
-    const getToThree = (e:any)=>{
-        setCurrent("three");
-        ref3.current?.scrollIntoView({behavior:"smooth"});
-    }
+  
     const getTabCallBack= (name: string, ref: any, stateCallback)=>{
         return (e)=>{
             stateCallback(name);
@@ -66,17 +44,16 @@ export const BurgerConstructor =(props: any)=>{
         }
     }
 
-
     const [ingredients, ingredientMap ]= getSortedData(getTestData, comparatorBurger);
-
-    // for(let i of ingredients){
-    //     refMap.set(i.type, useRef<HTMLDivElement>(null))
-    // }
-     
-   // ingredients.forEach(i=>{refMap.set(i.type, useRef<HTMLDivElement>(null))})
 
    const myRefs = useRef([]);
    myRefs.current =ingredients.map((element, i) => myRefs.current[i] ?? createRef());
+
+    const ingedientclicked = (number)=>{
+        return (e)=>{
+            console.log("clecked: ",number)
+        }
+    }
 
     return (
         <section>
@@ -89,43 +66,30 @@ export const BurgerConstructor =(props: any)=>{
                     </Tab>
                     )
                 })}
-                {/* <Tab value="one" active={current === 'one'} onClick={getToOne}>
-                    <h1 >One</h1>
-                </Tab>
-                <Tab value="two" active={current === 'two'} onClick={getToTwo}>
-                    <h1>Two</h1>
-                </Tab>
-                <Tab value="three" active={current === 'three'} onClick={getToThree}>
-                   <h1>Three</h1> 
-                </Tab> */}
+              
             </div>
             <div style={{height:'900px', overflow:'auto'}}>
                 {ingredients.map((ing,ind)=>{
                     return (
                         <div key={`${ing.type}`} id={`${ing.type}Type`} ref={ myRefs.current[ind]} >
                             <h2>{ing.type}</h2>
+                            <div style={{display:'flex', flexDirection:'row', flexWrap:'wrap', rowGap: '20px'}}>
                                 {ingredientMap.get(ing.type).map(ingred=>{
-                                    return (
-                                        <BurgerIngredient key={ingred.id} {...ingred}/>
+                                    const props = {
+                                        ...ingred,
+                                        clickCallback: ingedientclicked(ingred._id)
+                                    }
+                                    return (                                        
+                                             <BurgerIngredient key={ingred._id} { ...props } />                                       
                                     )
                                 })}
+                            </div>
                         </div>
                     )
                 })}
 
-                
-                {/* <div id="oneDiv" ref={ref1} style={{height:'600px', backgroundColor:'blue'}}>
-                    <h2>One</h2>
-                </div>
-                <div id="twoDiv" ref={ref2} style={{height:'600px', backgroundColor:'red'}}>
-                    <h2>Two</h2>
-                </div>
-                <div id="threeDiv" ref={ref3} style={{height:'600px', backgroundColor:'green'}}>
-                    <h2>Three</h2>
-                </div> */}
             </div>
         </section>
     )
 }
 
-//export default BurgerConstructor;
--- src/components/BurgerConstructor/BurgerIngredient.tsx
+++ src/components/BurgerConstructor/BurgerIngredient.tsx
@@ -1,12 +1,12 @@
 
 const BurgerIngredient = (props: any)=>{
     return (
-        <>
+        <div style={{width: '50%'}} onClick={props.clickCallback}>
             <img src={props.image} />
             <h2>{props.price}</h2>
             <h3>{props.name}</h3>
 
-        </>
+        </div>
 
     )
 }
