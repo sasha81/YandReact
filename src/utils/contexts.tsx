86c413504595b8215aa436ba07b8920c413137cb
SmartWebDeveloper10 <sashaeliseev81@gmail.com>
2022-04-04 17:37:05 +0300

    comments removed

--- src/utils/contexts.tsx
+++ src/utils/contexts.tsx
@@ -7,12 +7,7 @@ export interface IContextInteface {
     ingredientMap:IChoosenIngredients}
   
    
-// }
 
-// export type IngredientContextType={
-//     ingredients: IContextInteface,
-//     setIngredients: React.Dispatch<React.SetStateAction< IContextInteface>>
-// }
 export const initialIngredientValue = {bun:null, ingredients:[], ingredientMap:{}};
 //yes, yes, I know! This <any> is not the best thing here, but it drew me nuts to marry TS with the React Context. I didn't expect it to be THAT quirky!!!
 export const IngredientContext = React.createContext<any>({}  );
@@ -40,10 +35,10 @@ export const deleteIngredient = (ingredient:IBareBurgerIngredient, setIngredient
       else if(prev && prev.ingredientMap.hasOwnProperty(_id) && prev.ingredientMap[_id]===1){
         const obj = tempContext.ingredientMap;
         delete obj[_id];
-        //return obj
+       
       }
       else{
-        //return {...prev}
+      
       }
       const index = prev.ingredients.findIndex(el=>{return (el._id===_id)})
    
@@ -61,26 +56,17 @@ export const pickIngredient = (ingredient: IBareBurgerIngredient,setIngredientCo
           const tempContext = JSON.parse(JSON.stringify(prev))
               if(prev&& prev.ingredientMap.hasOwnProperty(_id)){
                 tempContext.ingredientMap[_id]=prev.ingredientMap[_id]+1;
-                // return {
-                  
-                //   ...prev,
-                //   [_id]: prev[_id]+1
-                // }
+              
               }
               else{
                 tempContext.ingredientMap[_id]=1;
-                // return {
-                //   ...prev,
-                //   [_id]:1
-                // }
+                
               }
               tempContext.ingredients= tempContext.ingredients.concat(ingredient);
               return tempContext;
             });
            
-            // setIngredientObjects((prev)=>{
-            //   return [...prev, ingredient]   
-            // });
+            
     }
       else if(ingredient.type==='bun' ){
         setIngredientContext(prev=>{
@@ -89,44 +75,16 @@ export const pickIngredient = (ingredient: IBareBurgerIngredient,setIngredientCo
           tempContext.bun={...ingredient};
           if(prev&& prev.ingredientMap.hasOwnProperty(_id)){
             tempContext.ingredientMap[_id]=prev.ingredientMap[_id]+1;
-            // return {
-              
-            //   ...prev,
-            //   [_id]: prev[_id]+1
-            // }
+           
           }
           else{
             tempContext.ingredientMap[_id]=1;
-            // return {
-            //   ...prev,
-            //   [_id]:1
-            // }
+           
           }
           return tempContext;
 
         })
-      }
-
-      //   setBun({...ingredient});
-      //   setIngredient(prev=>{     
-  
-      //     if(prev.hasOwnProperty(_id)){
-      //       return {
-      //         ...prev,
-      //         [_id]: prev[_id]+1
-      //       }
-      //     }
-      //     else{
-      //       return {
-      //         ...prev,
-      //         [_id]:1
-      //       }
-      //     }
-      //   });
-      // }
-      // else{
-  
-      // }
+      }     
   }
 
  export const orderComplete = (choosenIngredientObjects:IBareBurgerIngredient[],
